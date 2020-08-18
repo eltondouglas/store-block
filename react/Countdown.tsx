8fe30c055d298692f5f36946892fb0aae93c371a
eltondouglas <eltondouglas@live.com>
2020-08-18 20:06:23 -0300

    Countdown customizado

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { string } from 'prop-types'
 
-interface CountdownProps { }
+interface CountdownProps {
+    targetDate: string
+ }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+  return (
+    <div>
+      <h1>{targetDate}</h1>
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate:{
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: string,
+      default: null
+    }
+  },
 }
 
 export default Countdown
